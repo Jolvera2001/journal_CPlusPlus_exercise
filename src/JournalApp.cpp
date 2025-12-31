4b35ee82b93dd61817011772ad1aa9ca39c91294
Johan Olvera <olvjohan2001@gmail.com>
2025-12-31 12:40:14 -0600

    reorganizing project

--- src/JournalApp.cpp
+++ src/JournalApp.cpp
@@ -1,8 +1,13 @@
 #include "JournalApp.h"
 #include "JournalFrame.h"
+#include "wx/xrc/xmlres.h"
 
 bool JournalApp::OnInit()
 {
+    wxXmlResource::Get()->InitAllHandlers();
+
+    wxXmlResource::Get()->Load("Main.xrc");
+
     SetAppName("Journal");
 
     auto frame = new JournalFrame();
