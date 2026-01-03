a67a6d997c21f102da25efa5ab05f0d944ac123f
Johan Olvera <olvjohan2001@gmail.com>
2026-01-03 13:04:07 -0600

    added json project and implementing Database class

--- src/services/Database.cpp
+++ src/services/Database.cpp
@@ -2,15 +2,43 @@
 
 Database::Database(std::string &dbPath)
 {
-    int rc = sqlite3_open(dbPath.c_str(), &this->db);
-    // create handle based on default path
-    // instantiate tables
-    // assign handle to this->db
+    fs::path fullPath = fs::path(dbPath) / "journal.db";
+    int rc = sqlite3_open(fullPath.string().c_str(), &this->db);
+
+    if (rc != SQLITE_OK)
+    {
+        sqlite3_close(this->db);
+        this->db = nullptr;
+        throw std::runtime_error("Error initializing or connecting to database");
+    }
+
+    InitializeTables();
 };
 
 Database::~Database()
 {
-    if (this->db) {
+    if (this->db)
+    {
         sqlite3_close(db);
     }
 };
+
+void Database::InitializeTables()
+{
+    const char *sql =
+        "CREATE TABLE IF NOT EXISTS entries ("
+        "id INTEGER PRIMARY KEY AUTOINCREMENT"
+        "created_at TEXT NOT NULL"
+        "updated_at TEXT NOT NULL"
+        "title TEXT NOT NULL"
+        "content TEXT NOT NULL"
+        ");";
+
+    char *errMsg = nullptr;
+    int rc = sqlite3_exec(this->db, sql, nullptr, nullptr, &errMsg);
+
+    if (rc != SQLITE_OK)
+    {
+        sqlite3_free(errMsg);
+    }
+}
--- src/services/Database.h
+++ src/services/Database.h
@@ -1,7 +1,10 @@
 #pragma once
 #include <string>
+#include <filesystem>
 #include "sqlite3.h"
 #include "JournalEntryModel.h"
+
+namespace fs = std::filesystem;
 
 class Database
 {
@@ -9,10 +12,10 @@ public:
     Database(std::string &dbPath);
     ~Database();
 
-    Database(const Database&) = delete;
-    Database& operator=(const Database&) = delete;
+    Database(const Database &) = delete;
+    Database &operator=(const Database &) = delete;
 
-    sqlite3* GetHandle() { return db; }
+    sqlite3 *GetHandle() { return db; }
     int FindEntries();
     int SaveEntry(JournalEntry &newEntry);
     int UpdateEntry(int id, JournalEntry &newEntry);
@@ -20,4 +23,6 @@ public:
 private:
     sqlite3 *db;
     std::string dbPath;
+
+    void InitializeTables();
 };
