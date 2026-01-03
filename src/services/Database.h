92f8d35a22de521a793a0f7c7c9cdd068c87b5e9
Johan Olvera <olvjohan2001@gmail.com>
2026-01-03 00:10:09 -0600

    putting in database interface

--- src/services/Database.h
+++ src/services/Database.h
@@ -1,9 +1,15 @@
 #pragma once
+#include "sqlite3.h"
+#include "JournalEntryModel.h"
 
 class Database
 {
 public:
     Database();
-    
+    int FindEntries();
+    int SaveEntry(JournalEntry &newEntry);
+    int UpdateEntry(int id, JournalEntry &newEntry);
+
 private:
+    sqlite3 *db;
 };
